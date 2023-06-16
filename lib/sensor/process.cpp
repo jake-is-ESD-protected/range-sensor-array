1b90219ec661c1c900d439babf0a8b4b52262cb0
jake-is-ESD-protected <jt@tschavoll.at>
2023-06-16 19:28:11 +0200

    add parser and docs

--- lib/sensor/process.cpp
+++ lib/sensor/process.cpp
@@ -1,4 +1,6 @@
 #include "sensor_ir.h"
+#include "process.h"
+#include "MIDI.h"
 
 uint32_t get_min(uint32_t* array, uint32_t len){
     uint32_t min_val = array[0];
@@ -10,11 +12,40 @@ uint32_t get_min(uint32_t* array, uint32_t len){
 }
 
 void sensorLoop(void* p){
+
+    MIDI_CREATE_DEFAULT_INSTANCE();
+    MIDI.begin(MIDI_CHANNEL_OMNI);
+    Serial.begin(115200);
+    pinMode(SWITCH_PIN, INPUT);
+
     while(1){
         uint32_t start = trigger(1);
         vTaskDelay(100 / portTICK_PERIOD_MS);
         uint32_t min_dist = get_min(timings, NUM_SENSORS);
-        // do stuff with values idk
+
+        if(digitalRead(SWITCH_PIN)){
+            MIDI.sendControlChange(0, dist2midi(min_dist), 1);
+        }
+        else{
+            Serial.print("Distance = ");
+            Serial.print(min_dist);
+            Serial.println(" cm");
+        }
+        vTaskDelay(35 / portTICK_PERIOD_MS);
     }
-    
+}
+
+uint8_t dist2midi(uint32_t meas){
+    uint32_t dist = meas;
+    if(dist == 0){
+        dist = HIGH_MAP;
+    }
+    uint32_t cc = map(dist, LOW_MAP, HIGH_MAP, 0, 127);
+    if(cc > 127){
+        cc = 127;
+    }
+    if(cc < 0){
+        cc = 0;
+    }
+    return cc;
 }
--- lib/sensor/process.h
+++ lib/sensor/process.h
@@ -3,7 +3,46 @@
 
 #include <Arduino.h>
 
+#define LOW_MAP 0       // lowest distance value
+#define HIGH_MAP 250    // highest distance value (cm)
+#define SWITCH_PIN 15   // pin for manual serial/MIDI change
+
+/* Get smallest number (positive value) in array.
+ 
+* Parameters:
+    uint32_t* array: pointer to u-integer array
+    uint32_t len: length of array
+
+* Returns:
+    uint32_t smallest value in array
+*/
 uint32_t get_min(uint32_t* array, uint32_t len);
+
+/* Main sensor task (loop)
+ 
+* Parameters:
+    void* p: argument pointer (can be loaded from xTaskCreate)
+
+* Notes:
+    Does not stop. Initializes MIDI, Serial and the switch pin.
+    Sends a rising edge for 1 microsecond and waits for 100
+    milliseconds for all return to arrive. Calculates smallest
+    value from all sensor readings and converts it to CC messages
+    or Serial prints
+*/
 void sensorLoop(void* p);
+
+/* Convert distance to MIDI CC values.
+ 
+* Parameters:
+    uint32_t meas: measured distance
+
+* Returns:
+    uint8_t corresponding CC value
+
+* Notes:
+    Depends on LOW_MAP and HIGH_MAP macros.
+*/
+uint8_t dist2midi(uint32_t meas);
 
 #endif
